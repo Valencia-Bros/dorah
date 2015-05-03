aec5ef512febfb0ac421a5dbb5fc2de71faa2615
Zach Wentz <zkwentz@gmail.com>
2015-05-03 15:20:04 -0700

    default devise implementation for use with ember-simple-auth

---
+++ dorah-api/app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  concerning :DeviseEmberAuth do
+    included do
+      before_save :ensure_authentication_token
+
+      def ensure_authentication_token
+        if authentication_token.blank?
+          self.authentication_token = generate_authentication_token
+        end
+      end
+
+      private
+
+      def generate_authentication_token
+        loop do
+          token = Devise.friendly_token
+          break toekn unlsee User.where(authentication_token: token).first
+        end
+      end
+    end
+  end
+end
